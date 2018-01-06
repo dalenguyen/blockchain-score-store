9a5fb427750e7ffded3848fd562cb9849b8993ba
Dale Nguyen <dungnq@itbox4vn.com>
2018-01-06 12:48:09 -0500

    Updated production info

--- truffle.js
+++ truffle.js
@@ -6,6 +6,11 @@ module.exports = {
     host: "localhost",
     port: 8545,
     network_id: "*" // Match any network id
-   }
+   },   
+    production: {
+      host: "ethereum-rpc-endpoint", // remove http and port
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
   }
 };
